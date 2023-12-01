ba4b19386eed192be20ee1c557e2b16d4ae23e91
Roman Trippel <trippel@Romans-MBP.lan>
2023-11-30 19:19:19 -0800

    first commit

---
+++ src/main/java/com/romantrippel/langbook/LangbookApplication.java
@@ -0,0 +1,13 @@
+package com.romantrippel.langbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romantrippel/langbook/LangbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romantrippel.langbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
